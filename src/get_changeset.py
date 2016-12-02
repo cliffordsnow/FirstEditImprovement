3e6533e4489cad3212308781d8ff797a690b3364
Clifford Snow <clifford@snowandsnow.us>
2016-12-01 19:36:09 -0800

    added blank script for python code to get the changeset info using the api

---
+++ src/get_changeset.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+# use api call 
+# for example
+# http://api.openstreetmap.org/api/0.6/changeset/44046395?include_discussion=true
+# which returns
+# version="0.6" generator="CGImap 0.5.8 (16269 thorn-02.openstreetmap.org)" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
+# <changeset id="44046395" created_at="2016-11-29T22:16:22Z" closed_at="2016-11-29T22:16:24Z" open="false" user="321dy" uid="4900263" min_lat="47.7078253" min_lon="-122.0287069" max_lat="47.7078253" max_lon="-122.0287069" comments_count="1">
+# <tag k="created_by" v="MAPS.ME ios 6.5.3"/>
+# <tag k="comment" v="Created a community_centre"/>
+# <tag k="bundle_id" v="com.mapswithme.full"/>
+# <discussion>
+# <comment date="2016-11-30T00:40:50Z" uid="447903" user="Glassman">
+# <text>Is there a name to go with the community center?</text>
+# </comment>
+# </discussion>
+# </changeset>
+# </osm>
